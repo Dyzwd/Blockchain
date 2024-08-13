8186a3821f898d35a9813fb3ede1925d9f334c68
Dyzwd <149943318+Dyzwd@users.noreply.github.com>
2024-08-13 20:32:34 +0800

    Update trade.vue

--- blockchain_vue3/src/views/MainPages/trade.vue
+++ blockchain_vue3/src/views/MainPages/trade.vue
@@ -1,65 +1,118 @@
 <template>
-    <div class="top">
-        <el-button @click="add">新增订单</el-button>
-        <el-button @click="onDelete">删除订单</el-button>
-    </div>
-    <el-scrollbar max-height="600px">
-        <div v-for="item in count" :key="item" class="scrollbar-demo-item">
-            <el-descriptions title="订单号：" class="item">
-                <el-descriptions-item label="客户">kooriookami</el-descriptions-item>
-                <el-descriptions-item label="联系电话">18100000000</el-descriptions-item>
-                <el-descriptions-item label="地址">Suzhou</el-descriptions-item>
-                <el-descriptions-item label="订单状态">
-                    <el-tag size="small">已完成</el-tag>
-                </el-descriptions-item>
-                <el-descriptions-item label="详细地址">
-                    No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
-                </el-descriptions-item>
-            </el-descriptions>
+    <div class="container">
+        <div class="user-container">
+            <el-card shadow="hover">
+                <div class="user">
+                    <div class="user-left">
+                        <el-avatar shape="square" :size="80"
+                            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
+                    </div>
+                    <div class="user-right">
+                        <el-descriptions title="admin">
+                            <el-descriptions-item label="年龄：">35</el-descriptions-item>
+                            <el-descriptions-item label="电话：">181000000</el-descriptions-item>
+                            <el-descriptions-item label="邮箱：">123456789@qq.com</el-descriptions-item>
+                            <el-descriptions-item label="身份：">
+                                <el-tag size="small">种植户</el-tag>
+                            </el-descriptions-item>
+                            <el-descriptions-item label="地址">
+                                滨江区中信大道130号
+                            </el-descriptions-item>
+                        </el-descriptions>
+                    </div>
+                </div>
+            </el-card>
         </div>
-    </el-scrollbar>
+        <div class="trade-container">
+            <div class="trade-item-container" v-for="item in tradeData">
+                <el-card shadow="hover">
+                    <div class="trade-item">
+                        <el-descriptions title="User Info">
+                            <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
+                            <el-descriptions-item label="Telephone">18100000000</el-descriptions-item>
+                            <el-descriptions-item label="Place">Suzhou</el-descriptions-item>
+                            <el-descriptions-item label="Remarks">
+                                <el-tag size="small">School</el-tag>
+                            </el-descriptions-item>
+                            <el-descriptions-item label="Address">
+                                No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+                            </el-descriptions-item>
+                        </el-descriptions>
+                    </div>
+                </el-card>
+            </div>
+        </div>
+    </div>
+
+    <el-dialog v-model="dialogFormVisible" title="" width="500" center>
+        <el-form :model="form">
+            <el-form-item label="菜品" :label-width="formLabelWidth">
+                <el-input v-model="form.crop" autocomplete="off" />
+            </el-form-item>
+            <el-form-item label="重量" :label-width="formLabelWidth">
+                <el-input v-model="form.weight" autocomplete="off" />
+            </el-form-item>
+            <el-form-item label="地址" :label-width="formLabelWidth">
+                <el-input v-model="form.address" autocomplete="off" />
+            </el-form-item>
+            <!-- <el-form-item label="详细地址" :label-width="formLabelWidth">
+                <el-input v-model="form.address" autocomplete="off" />
+            </el-form-item> -->
+            <el-form-item label="交易状态" :label-width="formLabelWidth">
+                <el-input v-model="form.isTrade" autocomplete="off" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取消</el-button>
+                <el-button type="primary" @click="onSubmit">
+                    应用
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-const count = ref(3)
+import {ref,onMounted} from 'vue'
+let tradeData = ref([{}])
 
-const add = () => {
-    count.value++
-}
-const onDelete = () => {
-    if (count.value > 0) {
-        count.value--
-    }
-}
+//tradeData数据
+import tradeTable from '@/data/tradeTable';
+
+//生命周期
+onMounted(() => {
+    tradeData.value = tradeTable.tradeData.value
+})
 </script>
 
-<style scoped>
-.top {
-    margin-bottom: 20px;
-    padding: 10px;
-}
+<style scoped lang="less">
+.container {
 
-.scrollbar-demo-item {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-    margin: 20px;
-    border-radius: 4px;
-    background: var(--el-color-primary-light-9);
-    color: var(--el-color-primary);
+    .user-container {
 
-    .item {
-        width: 100%;
-        height: 100%;
-        padding: 20px;
-        background-color: #cccccc;
-        border-radius: 10px;
+        .user {
+            display: flex;
+            align-items: center;
 
-        >>> .el-descriptions__body {
-            background-color: yellowgreen;
+            .user-left {
+                height: 100%;
+                padding-right: 20px;
+                border-right: 1px solid #ccc;
+            }
+
+            .user-right {
+                margin-left: 20px;
+            }
         }
+    }
+
+    .trade-container{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 30px;
+        margin-top: 20px;
+        border-radius: 20px;
     }
 }
 </style>
