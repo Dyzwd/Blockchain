c026ea3909826e867c2e0cd22ea5363893f25870
Dyzwd <149943318+Dyzwd@users.noreply.github.com>
2024-08-23 15:39:04 +0800

    Update Login.vue
    换用新的登录界面

--- blockchain_vue3/src/views/Login.vue
+++ blockchain_vue3/src/views/Login.vue
@@ -1,364 +1,236 @@
-<template>
-    <div class="container">
-        <img src="/src/image/bg.jpg" alt="" class="bg">
-        <div class="main">
-            <div class="title"> 基于区块链地农产品溯源系统</div>
-            <div class="yhm-container">
-                <div class="yhm-srk">
-                    <img src="/src/image/icon-user.png" alt="" class="user-logo">
-                    <input type="text" class="yhm-input" placeholder="请输入用户名" v-model="yhm">
-                </div>
-            </div>
-            <div class="warm-container">
-                <div class="warm" :style="yhmWarm"> username is required</div>
-            </div>
-            <div class="mm-container">
-                <div class="mm-srk">
-                    <img src="/src/image/密码.png" alt="" class="mm-img">
-                    <input :type="wordShow" class="mm-input" placeholder="请输入密码" v-model="mm">
-                    <div class="eye" id="eye" @click="eyeChange">
-                        <img src="/src/image/眼睛_显示.png" alt="" :class="eyeClose ? 'eyes' : 'eyesActive'">
-                        <img src="/src/image/眼睛_隐藏.png" alt="" :class="eyeClose ? 'eyesActive' : 'eyes'">
-                    </div>
-                </div>
-            </div>
-            <el-form-item style="width: 200px">
-          <el-select placeholder="请选择角色" v-model="user">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
-        </el-form-item>
-            <div class="warm-container">
-                <div class="warm" :style="mmWarm"> password is required</div>
-            </div>
-            <div class="bt-container">
-                <div class="zc-bt" @click="register_a">注册</div>
-                <div class="dl-bt" @click="login_a">登录</div>
-            </div>
-        </div>
-        <div class="wz"></div>
-    </div>
-</template>
+<script setup>
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+//控制注册与登录表单的显示， 默认显示注册
+const isRegister = ref(false)
 
-<script setup lang="ts">
-import { ref, watch, reactive, computed } from 'vue';
-import { useAllDataStore } from '@/store'
-import { useRouter } from 'vue-router'
-import { register,login } from '@/request/request.ts'
-//密码小眼睛
+//用于注册的数据模型
+const registerData = ref({
+    username: '',
+    password: '',
+    rePassword: '',
+    role: ''
+})
 
-let options = [{
+//自定义确认密码的校验函数
+const rePasswordValid = (rule, value, callback) => {
+    if (value === "") {
+        callback(new Error("请再次输入密码"))
+    } else if (value != registerData.value.password) {
+        callback(new Error("两次输入的密码不一致"))
+    } else {
+        callback()
+    }
+}
+//用于注册的表单校验模型
+const registerDataRules = ref({
+    username: [
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+        { min: 5, max: 16, message: '用户名的长度必须为5~16位', trigger: 'blur' }
+    ],
+    password: [
+        { required: true, message: '请输入密码', trigger: 'blur' },
+        { min: 5, max: 16, message: '密码长度必须为5~16位', trigger: 'blur' }
+    ],
+    rePassword: [
+        { validator: rePasswordValid, trigger: 'blur' }
+    ],
+    role: [
+        { required: true, message: '请选择角色', trigger: 'blur' }
+    ],
+})
+
+//选择器
+const value = ref('')
+const options = [
+    {
         value: '种植户',
-        label: '种植户'
-      }, {
+        label: '种植户',
+    },
+    {
+        value: '物流',
+        label: '物流',
+
+    },
+    {
         value: '工厂',
-        label: '工厂'
-      }, {
-        value: '运输司机',
-        label: '运输司机'
-      }, {
-        value: '商店',
-        label: '商店'
-      }, {
-        value: '消费者',
-        label: '消费者'
-      }]
-let eyeClose = ref(1)
-let wordShow = ref('password')
+        label: '工厂',
 
-const router = useRouter()
-async function register_a(){
+    },
+    {
+        value: '店铺',
+        label: '店铺',
+
+    },
+    {
+        value: '管理员',
+        label: '管理员',
+        disabled: true,
+    }
+]
+
+//注册
+import { register,login } from '@/request/request.ts'
+import { ElMessage } from 'element-plus'
+const handleRegister = async () => {
     let formData = new FormData()
-    formData.append('username',yhm.value)
-    formData.append('password',mm.value)
-    formData.append('userType',user.value)
-    const result = await register(formData)
-    if (result == 1)
-    {
+    formData.append('username', registerData.value.username)
+    formData.append('password', registerData.value.password)
+    formData.append('role', registerData.value.role)
+    let result = await register(formData)
+    if (result == 1) {
         confirm('注册成功，交易id为' + localStorage.getItem('txid'))
+        ElMessage.success('注册成功')
     }
-    else
-    {
-        confirm('注册失败')
+    else {
+        /* confirm('注册失败') */
+        ElMessage.error('注册失败')
     }
-}
-function eyeChange() {
-    eyeClose.value = 1 - eyeClose.value
-}
-watch(eyeClose, (news) => {
-    if (news == 0) {
-        wordShow.value = 'text'
-    } else {
-        wordShow.value = 'password'
-    }
-})
-
-//warm事件
-let warmAllow = ref(0)
-let yhmWarm = reactive({
-    top: "-10px",
-    opacity: "0"
-})
-
-let yhm = ref('')
-let mmWarm = reactive({
-    top: "-10px",
-    opacity: "0"
-})
-let mm = ref('')
-let user = ref('')
-
-async function login_a() {
-    warmAllow.value = 1
-    if (yhm.value == '' || mm.value == '') {
-        if (yhm.value == '') {
-            Object.assign(yhmWarm, {
-                top: "0",
-                opacity: "1"
-            })
-        }
-        if (mm.value == '') {
-            Object.assign(mmWarm, {
-                top: "0",
-                opacity: "1"
-            })
-        }
-    } else {
-        let formData = new FormData()
-        formData.append('username',yhm.value)
-        formData.append('password',mm.value)
-        const result = await login(formData)
-        if (result == 1) {
-            router.push({ path: '/home/chart' })
-            localStorage.setItem('username',yhm.value)
-            confirm("登陆成功")
-        }else{
-            confirm("账号密码错误")
-        }
-    }
+    isRegister.value = false
+    clearInput();
 }
 
-watch(user,(news) => {
-    console.log('Selected user:', news);
+//清空函数
+const clearInput = () => {
+    registerData.value.username = ''
+    registerData.value.password = ''
+    registerData.value.rePassword = ''
+    registerData.value.role = ''
 }
 
-)
-watch(yhm, (news) => {
-    if (news == '' && warmAllow.value == 1) {
-        console.log('warm')
-        Object.assign(yhmWarm, {
-            top: "0",
-            opacity: "1"
-        })
+//登录-参数校验-同注册
+
+//登录
+import { useRouter } from 'vue-router'
+const router = useRouter() 
+const handleLogin = async() => {
+    let formData = new FormData()
+    formData.append('username', registerData.value.username)
+    formData.append('password', registerData.value.password)
+    let result = await login(formData)
+    if (result == 1) {
+        router.push({ path: '/home/chart' })
+        localStorage.setItem('username', registerData.value.username)
+        ElMessage.success("登录成功")
     } else {
-        Object.assign(yhmWarm, {
-            top: "-10px",
-            opacity: "0"
-        })
+        ElMessage.error("账号密码错误")
     }
-})
-
-watch(mm, (news) => {
-    if (news == '' && warmAllow.value == 1) {
-        console.log('warm')
-        Object.assign(mmWarm, {
-            top: "0",
-            opacity: "1"
-        })
-    } else {
-        Object.assign(mmWarm, {
-            top: "-10px",
-            opacity: "0"
-        })
-    }
-})
-
-
+}
 </script>
 
-<style scoped>
-.container {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    position: relative;
+
+
+<template>
+    <el-row class="login-page">
+        <el-col :span="12" class="bg"></el-col>
+        <el-col :span="6" :offset="3" class="form">
+            <h1 class="top-title">杭州市农产品朔源系统</h1>
+            <!-- 注册表单 -->
+            <el-form ref="form" size="large" autocomplete="off" v-if="isRegister" :model="registerData"
+                :rules="registerDataRules">
+                <el-form-item>
+                    <h1>注册</h1>
+                </el-form-item>
+                <el-form-item prop="username">
+                    <el-input :prefix-icon="User" placeholder="请输入用户名" v-model="registerData.username"></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input :prefix-icon="Lock" type="password" placeholder="请输入密码"
+                        v-model="registerData.password"></el-input>
+                </el-form-item>
+                <el-form-item prop="rePassword">
+                    <el-input :prefix-icon="Lock" type="password" placeholder="请输入再次密码"
+                        v-model="registerData.rePassword"></el-input>
+                </el-form-item>
+                <!-- @选择器 -->
+                <el-from-item prop="role">
+                    <el-select v-model="registerData.role" placeholder="请选择您的角色" style="margin-bottom: 20px;">
+                        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"
+                            :disabled="item.disabled" />
+                    </el-select>
+                </el-from-item>
+                <!-- 注册按钮 -->
+                <el-form-item>
+                    <el-button class="button" type="primary" auto-insert-space @click="handleRegister">
+                        注册
+                    </el-button>
+                </el-form-item>
+                <el-form-item class="flex">
+                    <el-link type="info" :underline="false" @click="isRegister = false;clearInput()">
+                        ← 返回
+                    </el-link>
+                </el-form-item>
+            </el-form>
+            <!-- 登录表单 -->
+            <el-form ref="form" size="large" autocomplete="off" v-else :model="registerData" :rules="registerDataRules">
+                <el-form-item>
+                    <h1>登录</h1>
+                </el-form-item>
+                <el-form-item prop="username">
+                    <el-input :prefix-icon="User" placeholder="请输入用户名" v-model="registerData.username"></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input name="password" :prefix-icon="Lock" type="password" placeholder="请输入密码"
+                        v-model="registerData.password"></el-input>
+                </el-form-item>
+                <el-form-item class="flex">
+                    <div class="flex">
+                        <el-checkbox>记住我</el-checkbox>
+                        <el-link type="primary" :underline="false">忘记密码？</el-link>
+                    </div>
+                </el-form-item>
+                <!-- 登录按钮 -->
+                <el-form-item>
+                    <el-button class="button" type="primary" auto-insert-space @click="handleLogin">登录</el-button>
+                </el-form-item>
+                <el-form-item class="flex">
+                    <el-link type="info" :underline="false" @click="isRegister = true;clearInput()">
+                        注册 →
+                    </el-link>
+                </el-form-item>
+            </el-form>
+        </el-col>
+    </el-row>
+</template>
+
+<style lang="less" scoped>
+/* 样式 */
+.login-page {
+    height: 100vh;
+    background-color: #fff;
 
     .bg {
-        filter: blur(6px);
-        position: absolute;
-        object-fit: cover;
-        justify-content: center;
+        background: url('@/assets/bg.png') no-repeat center / cover;
+        border-radius: 0 20px 20px 0;
     }
 
-    .main {
-        margin-top: 120px;
+    .form {
         display: flex;
         flex-direction: column;
-        align-items: center;
         justify-content: center;
-        z-index: 1;
+        user-select: none;
 
         .title {
-            font-size: 30px;
-            color: #545454;
-            margin-bottom: 20px;
+            margin: 0 auto;
+        }
+
+        .button {
+            width: 100%;
+        }
+
+        .flex {
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
         }
     }
 }
 
-
-
-
-.yhm-container {
-    width: 448.4px;
-    height: 52px;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    margin-top: 20px;
-    background-color: rgb(255, 255, 255, .3);
-    border-radius: 6px;
-    position: relative;
-    transition: .3s;
-}
-
-.yhm-container:hover {
-    filter: brightness(1.1);
-}
-
-.yhm-srk {
+.top-title{
     width: 100%;
-    height: 100%;
-    padding-left: 5px;
-    display: flex;
-    align-items: center;
-    border-radius: 10px;
-    overflow: hidden;
-}
-
-.yhm-input {
-    width: 100%;
-    height: 100%;
-    outline-style: none;
-    border: 0 none;
-    background: transparent;
-    font-size: 18px;
-}
-
-.mm-container {
-    width: 448.4px;
-    height: 52px;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    background-color: rgb(255, 255, 255, .3);
-    border-radius: 6px;
-    margin-top: 20px;
-    transition: .3s;
-}
-
-.mm-container:hover {
-    filter: brightness(1.1);
-}
-
-.mm-srk {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    border-radius: 10px;
-    overflow: hidden;
-    transition: .3s;
-    padding-left: 5px;
-    padding-right: 5px;
-}
-
-.mm-input {
-    width: 100%;
-    height: 100%;
-    outline-style: none;
-    border: 0 none;
-    background: transparent;
-    font-size: 18px;
-}
-
-.bt-container {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-top: 40px;
-}
-
-.bt-container div {
-    background: #409eff;
-    width: 135px;
-    height: 39.6px;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 14px;
-    color: #ffffff;
-    transition: .3s;
-}
-
-.bt-container div:first-child {
-    background: #909399;
-}
-
-.bt-container div:hover {
-    filter: brightness(1.1);
-}
-
-input[type="password"]::-ms-reveal {
-    display: none
-}
-
-.warm-container {
-    width: 448.4px;
-    height: 16px;
-    margin-top: 5px;
-    position: relative;
-}
-
-
-.warm {
-    color: #f56c6c;
-    font-size: 13px;
-    transition: .3s;
-    position: absolute;
-}
-
-.user-logo {
-    width: 30px;
-    height: 30px;
-    margin-left: 8px;
-    margin-right: 2px;
-}
-
-.mm-img {
-    width: 30px;
-    height: 30px;
-    margin-left: 10px;
-    margin-right: 2px;
-}
-
-.eyes {
-    display: none;
-}
-
-.eyesActive {
-    display: block;
-    width: 30px;
-    height: 30px;
-    margin-right: 8px;
+    text-align: center;
+    font-weight: 800;
+    font-size: 22px;
+    margin-bottom: 20px;
 }
 </style>
