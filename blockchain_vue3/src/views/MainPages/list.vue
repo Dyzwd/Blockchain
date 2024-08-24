dc28e58519edd3471a6a4e4239a0c23a70233cb3
Dyzwd <149943318+Dyzwd@users.noreply.github.com>
2024-08-24 19:37:22 +0800

    Update list.vue

--- blockchain_vue3/src/views/MainPages/list.vue
+++ blockchain_vue3/src/views/MainPages/list.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="right">
                 <div class="search">
-                    <el-input v-model="input" style="width: 240px" placeholder="请输入要查询的内容" clearable />
+                    <el-input v-model="serachInput" style="width: 240px" placeholder="请输入要查询的内容" clearable />
                     <el-button :icon="Search" type="primary" circle @click="onSearch" />
                 </div>
                 <div class="select">
@@ -28,52 +28,101 @@
             </div>
         </div>
         <div class="form">
-            <el-table :data="cropData" style="width: 100%">
+            <el-table :data="cropDataShow" style="width: 100%">
                 <el-table-column prop="farmer_input.fa_plantTime" label="日期" width="150" sortable />
                 <el-table-column prop="farmer_input.fa_fruitName" label="菜品" width="150" />
                 <el-table-column prop="farmer_input.fa_pickingTime" label="重量(kg)" width="150" />
                 <el-table-column prop="farmer_input.fa_origin" label="源产地" min-width="200" />
                 <el-table-column prop="traceability_code" label="溯源码" min-width="200" />
-                
                 <el-table-column label="管理" min-width="120">
                     <template #default="scope">
                         <el-button type="success" size="small" plain @click="handleEdit(scope.row)">
                             详细信息
                         </el-button>
-                        <!--el-button type="default" size="small" plain @click="handleDel(scope.row)">删除</el-button-->
                     </template>
                 </el-table-column>
             </el-table>
         </div>
         <div class="bottom">
-            <el-pagination size="default" background layout="prev, pager, next" :total="total" class="mt-4" @current-change="handleCurrentChange"/>
+            <el-pagination size="default" background layout="prev, pager, next" class="mt-4"/>
         </div>
 
     </div>
     <!-- dialog1 -->
-    <el-dialog v-model="dialogFormVisible" title="编辑" width="500" center>
-        <el-form :model="form">
-            <el-form-item  label="工厂信息"></el-form-item>
-            <el-form-item  >产品名称：{{datalabel.factory_input.fac_productName}}&nbsp&nbsp&nbsp&nbsp生产批次：{{datalabel.factory_input.fac_productionbatch}}&nbsp&nbsp&nbsp&nbsp生产时间：{{datalabel.factory_input.fac_productionTime}}&nbsp&nbsp&nbsp&nbsp工厂名称：{{datalabel.factory_input.fac_factoryName}}&nbsp&nbsp&nbsp&nbsp联系电话：{{datalabel.factory_input.fac_contactNumber}}</el-form-item>
-            <el-form-item label="司机信息"></el-form-item>
-        <el-form-item>司机姓名：{{datalabel.driver_input.dr_name}}&nbsp&nbsp&nbsp&nbsp司机年龄：{{datalabel.driver_input.dr_age}}&nbsp&nbsp&nbsp&nbsp司机电话：{{datalabel.driver_input.dr_phone}}&nbsp&nbsp&nbsp&nbsp
-  车辆车牌号：{{datalabel.driver_input.dr_carNumber}}&nbsp&nbsp&nbsp&nbsp
-  运输方式：{{datalabel.driver_input.dr_transport}}&nbsp&nbsp&nbsp&nbsp</el-form-item>
-<el-form-item label="商品信息"></el-form-item>
-<el-form-item>
-  上架时间：{{datalabel.shop_input.sh_storeTime}}&nbsp&nbsp&nbsp&nbsp
-  销售时间：{{datalabel.shop_input.sh_sellTime}}&nbsp&nbsp&nbsp&nbsp
-  商店名称：{{datalabel.shop_input.sh_shopName}}&nbsp&nbsp&nbsp&nbsp
-  商店地址：{{datalabel.shop_input.sh_shopAddress}}&nbsp&nbsp&nbsp&nbsp
-  联系电话：{{datalabel.shop_input.sh_shopPhone}}&nbsp&nbsp&nbsp&nbsp
-  
-</el-form-item>
+    <el-dialog v-model="dialogFormVisible" title="详情" width="800" center>
+        <el-form>
+            <el-descriptions title="种植园信息" :column="3" border>
+                <el-descriptions-item label="菜品" label-align="right" align="center" label-class-name="my-label"
+                    class-name="my-content" width="150px">
+                    {{ datalabel.farmer_input.fa_fruitName }}
+                </el-descriptions-item>
+                <el-descriptions-item label="朔源码" label-align="right" align="center" width="150px">
+                    {{ datalabel.traceability_code }}
+                </el-descriptions-item>
+                <el-descriptions-item label="播种时间" label-align="right" align="center" width="150px">
+                    {{ datalabel.farmer_input.fa_plantTime }}
+                </el-descriptions-item>
+                <el-descriptions-item label="采摘时间" label-align="right" align="center">
+                    {{ datalabel.farmer_input.fa_pickingTime }}
+                </el-descriptions-item>
+                <el-descriptions-item label="源产地" label-align="right" align="center">
+                    {{ datalabel.farmer_input.fa_origin }}
+                </el-descriptions-item>
+            </el-descriptions>
+            <el-descriptions title="工厂信息" :column="3" border style="margin-top: 10px;">
+                <el-descriptions-item label="工厂名" label-align="right" align="center" label-class-name="my-label"
+                    class-name="my-content" width="150px">
+                    {{ datalabel.factory_input.fac_factoryName }}
+                </el-descriptions-item>
+                <el-descriptions-item label="产品" label-align="right" align="center" width="150px">
+                    {{ datalabel.factory_input.fac_contactNumber }}
+                </el-descriptions-item>
+                <el-descriptions-item label="播种时间" label-align="right" align="center" width="150px">
+                    {{ datalabel.factory_input.fac_productionbatch }}
+                </el-descriptions-item>
+                <el-descriptions-item label="采摘时间" label-align="right" align="center">
+                    {{ datalabel.factory_input.fac_timestamp }}
+                </el-descriptions-item>
+                <el-descriptions-item label="源产地" label-align="right" align="center">
+                    {{ datalabel.factory_input.fac_productionTime }}
+                </el-descriptions-item>
+            </el-descriptions>
+            <el-descriptions title="物流信息" :column="3" border style="margin-top: 10px;">
+                <el-descriptions-item label="配送人" label-align="right" align="center" label-class-name="my-label"
+                    class-name="my-content" width="150px">
+                    {{ datalabel.driver_input.dr_name }}
+                </el-descriptions-item>
+                <el-descriptions-item label="联系电话" label-align="right" align="center" width="150px">
+                    {{ datalabel.driver_input.dr_phone }}
+                </el-descriptions-item>
+                <el-descriptions-item label="出发时间" label-align="right" align="center" width="150px">
+                    {{ datalabel.driver_input.dr_timestamp }}
+                </el-descriptions-item>
+                <el-descriptions-item label="运送方式" label-align="right" align="center">
+                    {{ datalabel.driver_input.dr_transport }}
+                </el-descriptions-item>
+            </el-descriptions>
+            <el-descriptions title="营销信息" :column="3" border style="margin-top: 10px;">
+                <el-descriptions-item label="店铺名" label-align="right" align="center" label-class-name="my-label"
+                    class-name="my-content" width="150px">
+                    {{ datalabel.shop_input.sh_shopName }}
+                </el-descriptions-item>
+                <el-descriptions-item label="工作时间" label-align="right" align="center" width="150px">
+                    {{ datalabel.shop_input.sh_selltime }}
+                </el-descriptions-item>
+                <el-descriptions-item label="联系电话" label-align="right" align="center" width="150px">
+                    {{ datalabel.shop_input.sh_shopPhone }}
+                </el-descriptions-item>
+                <el-descriptions-item label="店铺地址" label-align="right" align="center">
+                    {{ datalabel.shop_input.sh_shopAddress }}
+                </el-descriptions-item>
+            </el-descriptions>
         </el-form>
         <template #footer>
             <div class="dialog-footer">
                 <el-button @click="dialogFormVisible = false">取消</el-button>
-                <el-button type="primary" @click="onSubmit">
-                    区块链浏览器
+                <el-button type="primary" @click="dialogFormVisible = false">
+                    确认
                 </el-button>
             </div>
         </template>
@@ -82,111 +131,77 @@
 
 <script setup lang="ts">
 //附件区（一般不改）
-import { ref, onMounted, watch,reactive,nextTick } from 'vue'
+import { ref, watch } from 'vue'
 import { ArrowDown } from '@element-plus/icons-vue'
 import {
     Edit,
     Search,
 } from '@element-plus/icons-vue'
 import { getalldata } from '@/request/request.ts'
-const formLabelWidth = '70px'
-interface LinkItem {
-    value: string
-    link: string
-}
-const links = ref<LinkItem[]>([])
-const loadAll = () => {
-    return [
-        { value: 'vue', link: 'https://github.com/vuejs/vue' },
-        { value: 'element', link: 'https://github.com/ElemeFE/element' },
-        { value: 'cooking', link: 'https://github.com/ElemeFE/cooking' },
-        { value: 'mint-ui', link: 'https://github.com/ElemeFE/mint-ui' },
-        { value: 'vuex', link: 'https://github.com/vuejs/vuex' },
-        { value: 'vue-router', link: 'https://github.com/vuejs/vue-router' },
-        { value: 'babel', link: 'https://github.com/babel/babel' },
-    ]
-}
-const radio = ref('true')
+const serachInput = ref('')
 //工作区
 const dialogFormVisible = ref(false)
-let index = ref(0)
-let datalabel = ref([])
-const form = ref({
-    id: "",
-    crop: "",
-    source: "",
-    time: "",
-    weight: "",
-    isTrade: null,
+let datalabel = ref({
+    traceability_code:'',
+    shop_input:{},
+    factory_input:{},
+    driver_input:{},
+    farmer_input:{}
 })
-let cropData = reactive([{}])
+let cropData = ref([{}])
+let cropDataShow = ref([{}])
 //menuChange函数
 import useRoute from '@/hooks/useRoute'
 const { menuChange } = useRoute()
 
-//listTable数据
-import listTable from '@/data/lsitTable'
-import { handleCurrentChange } from 'element-plus/es/components/tree/src/model/util.mjs';
-
-//删除数据
-const handleDel = ({ id }: any) => {
-    index.value = listTable.cropData.value.findIndex(item => item.id === id)
-    listTable.cropData.value.splice(index.value, 1)
-}
-
 //编辑数据-打开
-const handleEdit = (res) => {
-    console.log(res)
-    datalabel.value = res
+const handleEdit = (res: any) => {
+    datalabel.value.traceability_code = res.traceability_code
+    datalabel.value.factory_input = res.factory_input
+    datalabel.value.farmer_input = res.farmer_input
+    datalabel.value.driver_input = res.driver_input
+    datalabel.value.shop_input = res.shop_input
     dialogFormVisible.value = true
-    //dialogFormVisible.value = true
+    console.log(datalabel.value)
+}
 
-    //index.value = listTable.cropData.value.findIndex(item => item.id === id)
-    //form.value = listTable.cropData.value[index.value]
-}
-//编辑数据-提交
-const onSubmit = () => {
-    dialogFormVisible.value = false
-    listTable.cropData.value[index.value] = form.value
-}
 
 //搜索
 let input = ref("")
 const onSearch = () => {
-    const searchValue = input.value;
-    const updatedData = cropData.filter(item => {
-        return item.traceability_code === searchValue;
-    });
-    cropData = updatedData;
-    nextTick(() => {
-        // 这里可以放置需要在DOM更新后执行的代码
-        console.log('表格应该已经更新了');
-    });
-    input.value = ""
-};
-
+    cropDataShow.value = cropData.value.filter(item => item.farmer_input.fa_fruitName.match(input.value))
+}
+watch(input, (news) => {
+    if (news === "") {
+        cropDataShow.value = cropData.value
+    }
+})
 
 //分页
-let total = ref(2)
-const handleCurrentChange = (val:any)=>{
-    cropData.value = listTable.cropData.value.filter(item => Number(item.id) > (val-1)*10 && Number(item.id) < val*10)
-}
 
-//生命周期
-onMounted(() => {
+//数据获取
+const getData = () => {
     getalldata()
     cropData = JSON.parse(localStorage.getItem('alldata'))
     cropData = cropData.filter(item => {
         return item.farmer_input.fa_fruitName != 'undefined' && item.farmer_input.fa_fruitName != ''
     })
-    console.log(cropData)
-    dialogFormVisible.value = true
-    dialogFormVisible.value = false
-    
-})
+    cropDataShow.value = cropData.value
+    console.log(cropData.value)
+}
+getData();
+
+
+
 </script>
 
 <style scoped>
+:deep(.my-label) {
+  background: var(--el-color-success-light-9) !important;
+}
+:deep(.my-content) {
+  background: var(--el-color-danger-light-9);
+}
 .container {
     width: 100%;
     height: 100%;
