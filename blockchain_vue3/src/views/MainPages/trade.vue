d76f8a050df4b93b9c843aa3b93a217f64ec7cda
taixiansen2 <taixiansen2@gmail.com>
2024-08-24 14:29:22 +0800

    Update trade.vue

--- blockchain_vue3/src/views/MainPages/trade.vue
+++ blockchain_vue3/src/views/MainPages/trade.vue
@@ -8,40 +8,43 @@
                             src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
                     </div>
                     <div class="user-right">
-                        <el-descriptions title="admin">
-                            <el-descriptions-item label="年龄：">35</el-descriptions-item>
-                            <el-descriptions-item label="电话：">181000000</el-descriptions-item>
-                            <el-descriptions-item label="邮箱：">123456789@qq.com</el-descriptions-item>
+                        <el-descriptions :title="sizeForm.name">
+                            <!--el-descriptions-item label="年龄：">35</el-descriptions-item>
+                            <el-descriptions-item label="电话：">181000000</el-descriptions-item-->
+                            
                             <el-descriptions-item label="身份：">
-                                <el-tag size="small">种植户</el-tag>
+                                <el-tag size="small">{{sizeForm.role}}</el-tag>
                             </el-descriptions-item>
-                            <el-descriptions-item label="地址">
-                                滨江区中信大道130号
-                            </el-descriptions-item>
+                            <el-descriptions-item label="用户ID">{{sizeForm.id}}</el-descriptions-item>
                         </el-descriptions>
                     </div>
-                    <div>
-                        <el-button @click="openDialog()">上传</el-button>
-                        <el-button @click="getData()">获取数据</el-button>
+                    <div style="margin-left: 350px">
+                        <el-button @click="openDialog()">发布订单</el-button>
+                        <!--el-button @click="getData()">获取数据</el-button-->
                     </div>
                 </div>
             </el-card>
         </div>
         <div class="trade-container">
-            <div class="trade-item-container" v-for="item in tradeData">
+            <div class="trade-item-container" v-for="order in tradeData">
                 <el-card shadow="hover">
                     <div class="trade-item">
-                        <el-descriptions title="User Info">
-                            <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
-                            <el-descriptions-item label="Telephone">18100000000</el-descriptions-item>
-                            <el-descriptions-item label="Place">Suzhou</el-descriptions-item>
-                            <el-descriptions-item label="Remarks">
-                                <el-tag size="small">School</el-tag>
+                        <el-descriptions :title="order.type">
+                            <el-descriptions-item label="订单价格:">{{ order.total_price }}</el-descriptions-item>
+
+                            <el-descriptions-item label="联系方式">{{ order.number }}</el-descriptions-item>
+                            
+                            <el-descriptions-item label="状态">
+                                <el-tag size="small">{{order.status}}</el-tag>
                             </el-descriptions-item>
-                            <el-descriptions-item label="Address">
-                                No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+                            <el-descriptions-item label="内容">
+                                {{order.content}}
                             </el-descriptions-item>
+
                         </el-descriptions>
+                        <el-button type="success" size="small" plain @click="handleEdit(scope.row)" style="margin-left: 350px;">
+                            接受订单
+                        </el-button>
                     </div>
                 </el-card>
             </div>
@@ -55,6 +58,9 @@
             </el-form-item>
             <el-form-item label="订单类型" >
                 <el-input  v-model="sizeForm.type" autocomplete="off" />
+            </el-form-item>
+            <el-form-item label="联系方式" >
+                <el-input  v-model="sizeForm.contact" autocomplete="off" />
             </el-form-item>
             <el-form-item label="订单内容" >
                 <el-input  v-model="sizeForm.content" autocomplete="off" />
@@ -93,7 +99,11 @@ const sizeForm = reactive({
     price:"",
     type:"",
     status:"",
-    content:""
+    content:"",
+    contact:"",
+    name:localStorage.getItem('username'),
+    role:localStorage.getItem('usertype'),
+    id:localStorage.getItem('userID'),
 
 })
 function getData(){
@@ -102,7 +112,7 @@ function getData(){
 async function upload(){
     console.log(sizeForm)
     let formData = new FormData()
-    formData.append('username',localStorage.getItem('userID'))
+    formData.append('number',sizeForm.contact)
     formData.append('price',sizeForm.price)
     formData.append('type',sizeForm.type)
     formData.append('status',sizeForm.status)
@@ -121,8 +131,9 @@ async function upload(){
     }
 }
 onMounted(() => {
-    
-    tradeData.value = tradeTable.tradeData.value
+    getorder()
+    tradeData.value = JSON.parse(localStorage.getItem('orders'))
+    console.log(tradeData)
     
 })
 </script>
